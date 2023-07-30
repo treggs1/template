adbf97943c0d1d46158c75106923954dcc014004
treggs1 <120253974+treggs1@users.noreply.github.com>
2023-07-30 13:25:37 +0000

    Add calc test

---
+++ scripts/calc.js
@@ -0,0 +1,6 @@
+function addition(a, b) {
+    let sum = a + b;
+    return sum;
+};
+
+module.exports = addition;
---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,28 @@
+/**
+ *  @jest-environment jsdom
+ */
+
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 70 for 42 + 28", () => {
+            expect(addition(42, 28)).toBe(70);
+        });
+    });
+
+    describe("Subtraction function", () => {
+
+    });
+
+    describe("Multiply function", () => {
+
+    });
+
+    describe("Division function", () => {
+
+    });
+})
